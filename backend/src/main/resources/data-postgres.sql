9b82d48261078fcfd91d9d29e8a83a75e3255bf5
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2021-11-16 12:08:19 +0100

    script fix

--- backend/src/main/resources/data-postgres.sql
+++ backend/src/main/resources/data-postgres.sql
@@ -1,8 +1,6 @@
 insert into zaposleni (ime, prezime, pol, datum_rodjenja, trenutna_plata, zaposlenje, putanja_slike, obrisan) values ('Perica', 'Peric', 0, '1990-01-01', 45000.0, 0, 'pera.jpg', false);
-insert into sankeri (id) values(1);
 
 insert into plate (pocetak_vazenja, zaposleni_id) values ('2021-11-11', 1);
 --
 insert into zaposleni (ime, prezime, pol, datum_rodjenja, trenutna_plata, zaposlenje, putanja_slike, obrisan) values ('Marko', 'Maric', 0, '1990-01-01', 60000.0, 3, 'marko.jpg', false);
-insert into kuvari (id) values(2);
 --insert into glavni_kuvari (korisnicko_ime, lozinka, id) values ('marko', 'marko', 2);
